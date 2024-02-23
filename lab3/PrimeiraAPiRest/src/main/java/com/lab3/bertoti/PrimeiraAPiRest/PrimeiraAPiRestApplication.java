a59b58c92c66b9c1d641a8e3e504e013e76f7165
Andre Bernardes <andreluiz1088@hotmail.com>
2024-02-23 20:50:13 -0300

    Começo da minha primeira api rest bertoti

---
+++ lab3/PrimeiraAPiRest/src/main/java/com/lab3/bertoti/PrimeiraAPiRest/PrimeiraAPiRestApplication.java
@@ -0,0 +1,13 @@
+package com.lab3.bertoti.PrimeiraAPiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraAPiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraAPiRestApplication.class, args);
+	}
+
+}
---
+++ lab3/PrimeiraAPiRest/src/test/java/com/lab3/bertoti/PrimeiraAPiRest/PrimeiraAPiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab3.bertoti.PrimeiraAPiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraAPiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
